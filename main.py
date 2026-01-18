6d50987d22228fac9dc8e530351c6abd3ad1e92a
AdaOkonkwo <ada.okonkwo@kodehauz.com>
2026-01-18 05:15:55 +0100

    updated CORS

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ app.add_middleware(
     allow_origins=[
         "https://legit-ink-tatoo.vercel.app/",
         "https://www.legitinktattoo.com"
-        "http://localhost:5500"],
+        "http://127.0.0.1:5500"],
     allow_methods=['GET', 'POST'],
     allow_headers=["*"],
 )
